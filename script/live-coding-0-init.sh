e3900a36609c10be44774892277d35975aa01098
Michael Kulla <michael.kulla@gedoplan.de>
2024-11-07 13:47:58 +0100

    Zeilenenden bei live-coding-0-init.sh auf LF geändert
    Wurde im letzten commit nicht übernommen

--- script/live-coding-0-init.sh
+++ script/live-coding-0-init.sh
@@ -1,1 +1,5 @@
-#!/bin/shMYDIR=`dirname "$0"`cd $MYDIR/..rm -fr src/main/java/de/gedoplan/Person*.java src/main/resources/application.properties src/test/java/*
+#!/bin/sh
+MYDIR=`dirname "$0"`
+
+cd $MYDIR/..
+rm -fr src/main/java/de/gedoplan/Person*.java src/main/resources/application.properties src/test/java/*
